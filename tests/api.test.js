13ace318174644cb084bd6443fbcc62674afa4c1
Pascal Iske <info@pascal-iske.de>
2018-01-14 23:26:01 +0100

    test(api): update api tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,30 +1,39 @@
-import test from 'ava';
-import { ProxyServer } from '../dist/index';
+import test from 'ava'
+import { ProxyServer } from '../dist/index'
 
 test('import is of type function', t => {
-    const value = typeof ProxyServer;
-    const expected = 'function';
+    const value = typeof ProxyServer
+    const expected = 'function'
 
-    t.is(value, expected);
-});
+    t.is(value, expected)
+})
 
 test('import has constructor function', t => {
-    const value = typeof ProxyServer.constructor;
-    const expected = 'function';
+    const value = typeof ProxyServer.constructor
+    const expected = 'function'
 
-    t.is(value, expected);
-});
+    t.is(value, expected)
+})
 
 test('import can be instanciated with constructor', t => {
-    const instance = new ProxyServer();
+    const instance = new ProxyServer()
 
-    const value1 = typeof instance;
-    const expected1 = 'object';
+    const value1 = typeof instance
+    const expected1 = 'object'
 
-    t.is(value1, expected1);
+    t.is(value1, expected1)
 
-    const value2 = typeof instance.constructor;
-    const expected2 = 'function';
+    const value2 = typeof instance.constructor
+    const expected2 = 'function'
 
-    t.is(value2, expected2);
-});
+    t.is(value2, expected2)
+})
+
+test('instanciated object has listen function', t => {
+    const instance = new ProxyServer()
+
+    const value = typeof instance.listen
+    const expected = 'function'
+
+    t.is(value, expected)
+})
