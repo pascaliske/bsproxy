1309bec4aa46dccd1c4c4d9895bd749ba389db32
Pascal Iske <info@pascal-iske.de>
2018-12-16 11:39:45 +0100

    chore(typescript): fix compiler issues

--- src/cli.ts
+++ src/cli.ts
@@ -3,21 +3,20 @@
 import { Logger, Arguments } from 'modern-cli'
 import { parse } from 'url'
 import { ProxyServer } from './lib/proxyserver'
-import * as pkg from '../package.json'
 
-// fetch package data
-const { name, version } = pkg as any
+// tslint:disable-next-line
+const pkg = require('../package.json')
 
 // enable logging
-process.env.DEBUG = name
+process.env.DEBUG = pkg.name
 
-const log: Logger = new Logger(name)
+const log: Logger = new Logger(pkg.name)
 const args: Arguments = new Arguments()
 const url: string = args.get(0) || false
 const port: number = args.get(1) || 9001
 
 // log version
-log(`v${version}`)
+log(`v${pkg.version}`)
 
 // check given url
 if (!url) {
